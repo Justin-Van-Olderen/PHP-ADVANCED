555e3eb691e8dad7319eea50819457713f83533d
Justin van Olderen <2107471@talnet.nl>
2020-09-02 14:24:12 +0200

    Opdracht 1 AF

---
+++ 1-Arrays/taak01/Index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Studenten</title>
+    
+</head>
+<body>
+    
+    <?php
+$voornaam = array("Piet", "Muhammad", "Els");
+$achternaam = array("alexander","Ali","Loodewijk");
+$leeftijd = array("18", "17", "16");
+$klas = array("9G","9G","9G");
+
+
+echo "Voornaam student is ". $voornaam['0'] ." Zijn achterernaam is " . $achternaam['0'] . " Zijn/haar leeftijd is ". $leeftijd['0'] . " En zit in Klas ". $klas['0'];
+
+?>
+    
+</body>
+</html>
